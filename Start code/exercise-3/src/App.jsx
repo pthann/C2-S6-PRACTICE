20cbd9c0ac8e6d9c20790eda191f1773ab399a50
Ha Thi <ha.pham25@student.passerellesnumeriques.org>
2024-06-08 10:22:42 +0700

    bai3

--- Start code/exercise-3/src/App.jsx
+++ Start code/exercise-3/src/App.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  /* You will need to use many state to keep the inut values and other needs */
+   /* You will need to use many state to keep the inut values and other needs */
+   /* You will need some function to handle the key pressed and button events */
+  const [valueA, setValueA] = useState("");
+  const [valueB, setValueB] = useState("");
+  const [result, setResult] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  /* You will need some function to handle the key pressed and button events */
+  const onA = (event) => {
+    setValueA(event.target.value);
+  };
+
+  const onB = (event) => {
+    setValueB(event.target.value);
+  };
+
+  const isNumber = (text) => {
+    return !isNaN(text) && text.trim() !== "";
+  };
+  const onCompute = () => {
+    if (isNumber(valueA) && isNumber(valueB)) {
+      const sum = parseFloat(valueA) + parseFloat(valueB);
+      setResult(sum);
+      setIsError(false);
+    } else {
+      setResult("Error: A and B shall be numbers");
+      setIsError(true);
+    }
+  };
 
   return (
     <main>
@@ -17,11 +42,16 @@ function App() {
 
       <label>A + B =</label>
 
-      {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      {/* When Compute button is clicked, this input displays the sum of the 2 numbers, or the error message in RED */}
+      <input
+        disabled
+        value={result}
+        style={{ color: isError ? "red" : "black" }}
+      />
+      <button onClick={onCompute}>Compute</button>
     </main>
   );
 }
 
 export default App;
+
